c5b982b0ffcb75a2fb642ffd4c38266c05ec981d
ML <me.mlaure@gmail.com>
2023-07-22 02:17:13 +0100

    make lean

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  umami: {
+    debug: true,
+    version: 2,
+    host: 'https://analytics.umami.is/',
+    id: 'bd613cc3-94a6-42f1-b1d2-c6370965b7d6',
+    autoTrack: true,
+  },
+})
